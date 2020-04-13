71b6893a98e7e6dcbefea1b00701f4e8e09aac94
SChakravorti21 <shoumyoarchit@gmail.com>
2020-04-13 15:26:20 -0400

    Collecting better list of Kpop artists

--- data_collection/collect_artists.py
+++ data_collection/collect_artists.py
@@ -2,42 +2,57 @@ import os
 import errno
 import sys
 import json
+import math
 import spotipy
 import utils
 from spotipy.oauth2 import SpotifyClientCredentials
 
-
 SEARCH_LIMIT = 50
-GENRES       = { "k-pop"           : 50,
-                 "k-pop+boy+group" : 42, 
-                 "k-pop+girl+group": 33,
-                 "pop"             : 50 }
-GENRE_FILES  = { "k-pop"           : "data/kpop.json",
-                 "k-pop+boy+group" : "data/kpop-bg.json", 
-                 "k-pop+girl+group": "data/kpop-gg.json",
-                 "pop"             : "data/pop.json" }
+GENRE_FILES  = { "k-pop"    : "data/kpop.json",
+                 "k-pop-lg" : "data/kpop-lg.json", 
+                 "pop"      : "data/pop.json" }
 
 
-sp = utils.get_spotipy_instance()
-artists_by_genre = {}
+def collect_artists(genre, num_artists=SEARCH_LIMIT):
+    sp = utils.get_spotipy_instance()
+    artists = []
+
+    for batch in utils.batches(range(num_artists), SEARCH_LIMIT):
+        search_results = sp.search(
+            q=f"genre:{genre}",
+            type="artist",
+            offset=batch[0],
+            limit=len(batch)
+        )
+
+        artists += search_results["artists"]["items"]
+    
+    return sorted(
+        artists,
+        reverse=True, 
+        key=lambda artist: artist["popularity"]
+    )
 
 
-for genre, cutoff in GENRES.items():
-    search_results = sp.search(
-        q=f"genre:{genre}",
-        type="artist",
-        limit=50)
+def collect_pop_kpop_artists():
+    # Collect top 50 K-pop artists
+    kpop_artists = collect_artists("k-pop")
+    kpop_artist_names = set(a["name"] for a in kpop_artists)
+    
+    # Collect top 50 pop artists, filter out
+    # any artists who also showed up under K-pop 
+    pop_artists = [a for a in collect_artists("pop") 
+                   if a["name"] not in kpop_artist_names]
 
-    artists = search_results["artists"]["items"]
-    artists = sorted(artists,
-                    reverse=True, 
-                    key=lambda artist: artist["popularity"])
-    artists = artists[:cutoff]
-    artists_by_genre[genre] = set(artist["name"] for artist in artists)
+    utils.write_json(pop_artists, GENRE_FILES["pop"])
+    utils.write_json(kpop_artists, GENRE_FILES["k-pop"])
 
-    # Some k-pop groups show up under the pop category as well
-    if genre == "pop":
-        artists = [artist for artist in artists 
-                   if artist["name"] not in artists_by_genre["k-pop"]]
 
-    utils.write_json(artists, GENRE_FILES[genre])
+def collect_many_kpop_artists():
+    artists = collect_artists("k-pop", num_artists=100)
+    utils.write_json(artists, GENRE_FILES["k-pop-lg"])
+
+
+if __name__ == "__main__":
+    collect_pop_kpop_artists()
+    collect_many_kpop_artists()
