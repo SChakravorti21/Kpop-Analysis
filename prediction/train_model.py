2145772330d405d85e30b3900910ba7def34e2ac
SChakravorti21 <shoumyoarchit@gmail.com>
2020-05-08 23:14:42 -0400

    Able to get 70% out of GBT and NN consistently

--- prediction/train_model.py
+++ prediction/train_model.py
@@ -1,9 +1,14 @@
 import os
+import sys
+import utils
+import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from enum import Enum
 from pprint import pprint
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
@@ -31,29 +36,97 @@ FEATURES = ["danceability",
             "tempo",
             "explicit"]
 
-input_path = os.path.join("data", "playlist_features.json")
-features = pd.read_json(input_path)
+class ModelType(Enum):
+    GradientBoosting    = 0
+    NeuralNetwork       = 1
+    KNearestNeighbors   = 2
 
-X = features[FEATURES]
-y = features["playlist"].map(CATEGORIES)
 
-pipe = Pipeline([
-    ("one-hot", ColumnTransformer([
-        ("key_category", preprocessing.OneHotEncoder(handle_unknown="ignore"), ["key"])
-    ], remainder="passthrough")),
-    ("scale", preprocessing.MinMaxScaler()),
-    ("select", feature_selection.SelectKBest()),
-    ("model", GradientBoostingClassifier())
-])
+class PlaylistClassifier():
+    def __init__(self, model_type: ModelType):
+        input_path = os.path.join("data", "playlist_features.json")
+        features = pd.read_json(input_path)
 
-param_grid = {
-    "select__k": [5, 6, 7, 8, 9],
-    "model__n_estimators": [50, 60, 70, 80, 100, 150, 175, 200],
-    # "model__min_samples_split": [2, 0.05, 0.1, 0.2],
-    "model__max_depth": [1, 2]
-}
+        self.model_type = model_type
+        self.X = features[FEATURES]
+        self.y = features["playlist"].map(CATEGORIES)
 
-search = GridSearchCV(pipe, param_grid, cv=10, n_jobs=-1, verbose=10).fit(X, y)
-clf = search.best_estimator_
-print("\n\nAccuracy: %0.2f " % (search.best_score_))
-print(clf)
+    def train(self):
+        if self.model_type == ModelType.GradientBoosting:
+            pipe, param_grid = self._get_gbt_pipeline()
+            model_path = os.path.join("prediction", "models", "gbt")
+        elif self.model_type == ModelType.NeuralNetwork:
+            pipe, param_grid = self._get_nn_pipeline()
+            model_path = os.path.join("prediction", "models", "nn")
+
+        # Perform exhaustive grid search to find the best model
+        search = GridSearchCV(pipe, param_grid, cv=10, 
+                              n_jobs=-1, verbose=2)
+        search = search.fit(self.X, self.y)
+        clf = search.best_estimator_
+        
+        # Just to see what were the optimal hyperparameters
+        print("\n\nAccuracy: %0.2f " % (search.best_score_))
+        print(clf[-2])
+        print(clf[-1])
+
+        # Save best model to disk
+        clf = clf.fit(self.X, self.y)
+        utils.makedirs(model_path)
+        joblib.dump(clf, model_path)
+
+    def _get_gbt_pipeline(self):
+        pipe = Pipeline([
+            ("one-hot", ColumnTransformer([
+                ("key_category", 
+                 preprocessing.OneHotEncoder(handle_unknown="ignore"), 
+                 ["key"])
+            ], remainder="passthrough")),
+            ("scale", preprocessing.MinMaxScaler()),
+            ("select", feature_selection.SelectKBest()),
+            ("model", GradientBoostingClassifier())
+        ])
+
+        param_grid = {
+            "select__k": [5, 6, 7, 8, 9],
+            "model__n_estimators": [75, 100, 150, 175, 200, 250],
+            "model__min_samples_split": [2, 0.05, 0.1, 0.2],
+            "model__max_depth": [1, 2]
+        }
+
+        return pipe, param_grid
+
+    def _get_nn_pipeline(self):
+        pipe = Pipeline([
+            ("one-hot", ColumnTransformer([
+                ("key_category", 
+                 preprocessing.OneHotEncoder(handle_unknown="ignore"), 
+                 ["key"])
+            ], remainder="passthrough")),
+            ("scale", preprocessing.StandardScaler()),
+            ("select", feature_selection.SelectKBest()),
+            ("model", MLPClassifier(solver="adam", max_iter=5000, 
+                                    early_stopping=True, n_iter_no_change=5))
+        ])
+
+        param_grid = {
+            "select__k": [4, 6, 8],
+            "model__alpha": 10.0 ** -np.arange(7, 12),
+            "model__hidden_layer_sizes": [(500,), (500, 100), (500, 500)],
+            "model__activation": ["relu", "tanh"],
+            "model__learning_rate_init": 10.0 ** -np.arange(1, 6)
+        }
+
+        return pipe, param_grid
+
+
+if __name__ == "__main__":
+    model_type = sys.argv[1]
+    
+    if model_type == "gbt":
+        model_type = ModelType.GradientBoosting
+    elif model_type == "nn":
+        model_type = ModelType.NeuralNetwork
+
+    classifier = PlaylistClassifier(model_type)
+    classifier.train()
